8035b972a0d88a187275118d2bfd90bb8468dd74
Pedro Santana <pedromanu27@gmail.com>
2022-12-17 22:46:54 -0300

    cloud management react app

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,72 @@
+import { CheckIcon } from '@heroicons/react/24/outline'
+import React from 'react'
+
+export const AllInOne = () => {
+    return (
+        <div name='platforms' className='w-full my-32'>
+            <div className='max-w-[1240px} mx-auto px-2'>
+                <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+                <p className='text-2xl py-8 text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad ea numquam doloremque sed repudiandae.</p>
+
+                <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+                    <div className='flex'>
+                        <div className=''>
+                            <CheckIcon className='w-7 mr-4 text-green-500' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi id dicta numquam, temporibus autem odit?</p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div className=''>
+                            <CheckIcon className='w-7 mr-4 text-green-500' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi id dicta numquam, temporibus autem odit?</p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div className=''>
+                            <CheckIcon className='w-7 mr-4 text-green-500' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi id dicta numquam, temporibus autem odit?</p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div className=''>
+                            <CheckIcon className='w-7 mr-4 text-green-500' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi id dicta numquam, temporibus autem odit?</p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div className=''>
+                            <CheckIcon className='w-7 mr-4 text-green-500' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi id dicta numquam, temporibus autem odit?</p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div className=''>
+                            <CheckIcon className='w-7 mr-4 text-green-500' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi id dicta numquam, temporibus autem odit?</p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
+
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+export const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+    const handleClose = () => setNav(!nav)
+
+    return (
+        <div className='w-screen h-[80px] bg-zinc-200 fixed drop-shadow-lg z-10'>
+            <div className="px-2 flex justify-between items-center w-full h-full">
+                <div className='flex items-center'>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
+                    <ul className="hidden md:flex">
+                        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+                        <li><Link to="about" smooth={true} offset={-200} duration={500}>Sobre</Link></li>
+                        <li><Link to="support" smooth={true} offset={-50} duration={500}>Suporte</Link></li>
+                        <li><Link to="platforms" smooth={true} offset={-100} duration={500}>Plataformas</Link></li>
+                        <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Preços</Link></li>
+                    </ul>
+                </div>
+                <div className="hidden md:flex pr-4">
+                    <button className='border-none bg-transparent text-black mr-4'>Sign In</button>
+                    <button className='px-8 py-3'>Sign Up</button>
+                </div>
+                <div className='md:hidden mr-4' onClick={handleClick}>
+                    {!nav ? <Bars3Icon className='w-5' /> : <XMarkIcon className='w-5' />}
+                </div>
+            </div>
+
+            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>Sobre</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Suporte</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="platforms" smooth={true} offset={-100} duration={500}>Plataformas</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Preços</Link></li>
+                <div className='flex flex-col my-4'>
+                    <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+                    <button className='px-8 py-3'>Sign Up</button>
+                </div>
+            </ul>
+
+        </div>
+    )
+}
